025c6b7cee29304218382bbef8e325b29b21852c
Amrish <amrish.m004@gmail.com>
2024-08-20 00:27:45 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/dev/amrish/productservice/Configurations/SecurityConfig.java
@@ -0,0 +1,19 @@
+package dev.amrish.productservice.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
